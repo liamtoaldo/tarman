3dffdff1970f5ee9235c9fabdcf1c16e11bdc3e1
Alessandro-Salerno <oxygen05@outlook.it>
2024-10-29 11:47:25 +0100

    beginning of package structure

---
+++ include/package.h
@@ -0,0 +1,43 @@
+/*************************************************************************
+| tarman                                                                 |
+| Copyright (C) 2024 Alessandro Salerno                                  |
+|                                                                        |
+| This program is free software: you can redistribute it and/or modify   |
+| it under the terms of the GNU General Public License as published by   |
+| the Free Software Foundation, either version 3 of the License, or      |
+| (at your option) any later version.                                    |
+|                                                                        |
+| This program is distributed in the hope that it will be useful,        |
+| but WITHOUT ANY WARRANTY; without even the implied warranty of         |
+| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          |
+| GNU General Public License for more details.                           |
+|                                                                        |
+| You should have received a copy of the GNU General Public License      |
+| along with this program.  If not, see <https://www.gnu.org/licenses/>. |
+*************************************************************************/
+
+#pragma once
+
+#include <stdbool.h>
+
+// Contents of .tmpkg files
+typedef struct {
+  const char *url;
+  const char *from_repoistory;
+  const char *application_name;
+  const char *executable_path;
+  const char *working_directory;
+  const char *icon_path;
+} pkg_info_t;
+
+// Contents of .tmrecepie files
+typedef struct {
+  pkg_info_t pkg_info;
+  bool       add_to_path;
+  bool       add_to_desktop;
+} recepie_t;
+
+typedef struct {
+  pkg_info_t pkg_info;
+  // ...
+} pkg_t;
