3c9869673551a2a726cf2d32758d5f12c4e9eb8a
Alessandro-Salerno <OxyGen05@outlook.it>
2024-11-05 16:17:23 +0100

    first part of download

---
+++ src/common/download.c
@@ -0,0 +1,32 @@
+/*************************************************************************
+| tarman                                                                 |
+| Copyright (C) 2024 Alessandro Salerno                                  |
+|                                                                        |
+| This program is free software: you can redistribute it and/or modify   |
+| it under the terms of the GNU General Public License as published by   |
+| the Free Software Foundation, either version 3 of the License, or      |
+| (at your option) any later version.                                    |
+|                                                                        |
+| This program is distributed in the hope that it will be useful,        |
+| but WITHOUT ANY WARRANTY; without even the implied warranty of         |
+| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          |
+| GNU General Public License for more details.                           |
+|                                                                        |
+| You should have received a copy of the GNU General Public License      |
+| along with this program.  If not, see <https://www.gnu.org/licenses/>. |
+*************************************************************************/
+
+#include "download.h"
+#include <stdlib.h>
+
+#include "os/exec.h"
+#include "os/fs.h"
+#include "plugin/plugin.h"
+
+bool download(const char *dst, const char *url) {
+  if (plugin_exists("download-plugin")) {
+    return EXIT_SUCCESS == plugin_run("download-plugin", dst, url);
+  }
+
+  // do stuff here
+}
