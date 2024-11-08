b3fc172f63fe451ec58bb0876afcfd455113ed2e
Alessandro Salerno <67583725+Alessandro-Salerno@users.noreply.github.com>
2024-11-08 01:20:42 +0100

    Update upload.sh

--- .github/workflows/upload.sh
+++ .github/workflows/upload.sh
@@ -1,7 +1,3 @@
-git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
-git config --local user.name "github-actions[bot]"
-git checkout --orphan latest-build
-
 git rm -rf *.md
 git rm -rf .github
 git rm -rf include
@@ -12,18 +8,6 @@ git rm -rf .clang-format
 git rm -rf Makefile
 git rm -rf plugins
 git rm -rf compile_flags.txt
-
-mkdir $1
-cp ./bin/tarman $1/tarman
-cp -r ./bin/plugin $1/plugins
-cp -r ./bin/plugin-sdk.o $1/plugin-sdk.o
-
-mkdir tarman
-cp ./bin/tarman ./tarman/tarman
-tar -cvzf ./$1/tarman-$1.tar.gz ./tarman
-rm -rf ./tarman
-
-git add $1/
 
 git commit -m "new bundle"
 git push --set-upstream origin latest-build --force
