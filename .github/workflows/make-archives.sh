5f316de67ef867c30ab9ec4922b3f0c0ab8c0ff7
Alessandro Salerno <67583725+Alessandro-Salerno@users.noreply.github.com>
2024-11-08 01:20:35 +0100

    Create make-archives.sh

---
+++ .github/workflows/make-archives.sh
@@ -0,0 +1,15 @@
+git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git config --local user.name "github-actions[bot]"
+git checkout --orphan latest-build
+
+mkdir $1
+cp ./bin/tarman $1/tarman
+cp -r ./bin/plugin $1/plugins
+cp -r ./bin/plugin-sdk.o $1/plugin-sdk.o
+
+mkdir tarman
+cp ./bin/tarman ./tarman/tarman
+tar -cvzf ./$1/tarman-$1.tar.gz ./tarman
+rm -rf ./tarman
+
+git add $1/
