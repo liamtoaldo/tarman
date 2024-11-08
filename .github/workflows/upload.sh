2954e275fce34949bf6fe7007b2307a367b73d30
Alessandro Salerno <67583725+Alessandro-Salerno@users.noreply.github.com>
2024-11-08 01:38:09 +0100

    Update upload.sh

--- .github/workflows/upload.sh
+++ .github/workflows/upload.sh
@@ -6,7 +6,7 @@ git rm -rf .gitattributes
 git rm -rf .gitignore
 git rm -rf .clang-format
 git rm -rf Makefile
-git rm -rf plugins
+git rm -rf ./plugins
 git rm -rf compile_flags.txt
 
 git commit -m "new bundle"
