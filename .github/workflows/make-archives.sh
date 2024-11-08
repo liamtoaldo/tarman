be85f9bf10c32cc15e96b6b82c78d696147cb3a7
Alessandro Salerno <67583725+Alessandro-Salerno@users.noreply.github.com>
2024-11-08 01:38:53 +0100

    Update make-archives.sh

--- .github/workflows/make-archives.sh
+++ .github/workflows/make-archives.sh
@@ -4,7 +4,7 @@ git checkout --orphan latest-build
 
 mkdir $1
 cp ./bin/tarman $1/tarman
-cp -r ./bin/plugin $1/plugins
+cp -r ./bin/plugins $1/plugins
 cp -r ./bin/plugin-sdk.o $1/plugin-sdk.o
 
 mkdir tarman
